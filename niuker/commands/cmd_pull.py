e90b77526bed6702ecc84da9e00f9baf3f0e4ad6
niusmallnan <zhangzb@neunn.com>
2016-02-25 07:38:28 +0000

    just pull from alauda friendly

--- niuker/commands/cmd_pull.py
+++ niuker/commands/cmd_pull.py
@@ -18,23 +18,45 @@ import click
 from niuker.cli import pass_context
 from sh import docker
 
+NEUNN_REGISTRY = 'index.neunn.com'
+ALAUDA_REGISTRY = 'index.alauda.cn'
+
+def parse_alauda_image(image, private_registry):
+    if image.find('/') > 0:
+        pulled_image = '%s/%s' % (private_registry, image)
+    else:
+        pulled_image = '%s/library/%s' % (private_registry, image)
+    return pulled_image
+
+def parse_neunn_image(image, private_registry):
+    pass
+
+
+def parse_image(image, private_registry):
+    if private_registry == ALAUDA_REGISTRY:
+        return parse_alauda_image(image, private_registry)
+    return image
+
+
 @click.command('pull', short_help='pull images from private registry')
 @click.argument('images', nargs=-1)
 @click.option('--private_registry', envvar='NIUKER_PRIVATE_REGISTRY',
               metavar='private_registry',
               help='set the private registry domain')
 @pass_context
-def cli(ctx, images, private_registry='index.neunn.com'):
+def cli(ctx, images, private_registry):
     """pull images from private registry
 
     images could be a list param
     """
     ctx.log('pull %s from %s' % (images, private_registry))
     for image in images:
+        image = parse_image(image, private_registry)
         ctx.log('RUN: docker pull %s' % image)
         docker.pull(image)
-        new_image = image.replace(private_registry, '')[1:]
-        docker.tag(image, new_image)
-        docker.rmi(image)
+        if private_registry:
+            new_image = image.replace(private_registry, '')[1:]
+            docker.tag(image, new_image)
+            docker.rmi(image)
 
     print(docker.images())

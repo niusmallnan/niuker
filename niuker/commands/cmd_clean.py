0612027b3a19d90e7e94c55b1ba91c7e8e3bb142
niusmallnan <zhangzb@neunn.com>
2016-04-29 09:01:15 +0000

    optimize clean images

--- niuker/commands/cmd_clean.py
+++ niuker/commands/cmd_clean.py
@@ -18,6 +18,7 @@ import click
 from niuker.cli import pass_context
 from sh import docker
 
+
 @click.group()
 def cli():
     """clean docker containers or images"""
@@ -47,24 +48,30 @@ def containers(ctx, force, all):
     print(docker.ps('-a'))
 
 
+def remove_images(ctx, images, force=False):
+    for image in images:
+        ctx.log('docker removing image %s' % image)
+        if force:
+            print(docker.rmi('-f', image))
+        else:
+            print(docker.rmi(image))
+
+
 @cli.command()
 @click.option('--force', '-f', is_flag=True, help='force remove images')
 @click.option('--all', '-a', is_flag=True, help='remove all the images')
 @pass_context
 def images(ctx, force, all):
     """clean docker images which has no tag"""
+    ctx.log('clean untag images')
+    untag_images = docker.images('--format','{{.Repository}}:{{.Tag}}',
+                                 "--filter='dangling=true'", '-q').split()
+    remove_images(ctx, untag_images, force)
     if all:
         ctx.log('clean all images')
-        removed_images = docker.images('-q')
-    else:
-        ctx.log('clean images only no tag')
-        removed_images = docker.images("--filter='dangling=true'", '-q')
-    for img_id in removed_images.split():
-        ctx.log('docker removing image %s' % img_id)
-        if force:
-            print(docker.rmi('-f', img_id))
-        else:
-            print(docker.rmi(img_id))
+        all_images = docker.images('--format','{{.Repository}}:{{.Tag}}',
+                                   '-q').split()
+        remove_images(ctx, all_images)
 
     ctx.log('list all images')
     print(docker.images())
